5cbd61d421cc591b98dea4bc7f38d37a673ed134
Unknown <arthfind@gmail.com>
2019-09-05 16:45:53 +0200

    Separate encoder part and global + 3D files

--- Motor/DC_Motor_Encoder/DC_Motor_Encoder.ino
+++ Motor/DC_Motor_Encoder/DC_Motor_Encoder.ino
@@ -1,5 +1,11 @@
+/**
+ * Run on arduino nano to controle and measure DC motor's speed
+ *
+ * author : Arthur FINDELAIR, github.com/ArthurFDLR
+ * date : September 2019
+ */
+
 #include "src/DC_motor_driver/DC_motor_driver.h"
-#include "src/Encoder/Encoder.h"
 
 //#include <Ewma.h> // Filtering library
 
@@ -12,6 +18,9 @@
 unsigned long timePrev = 0;
 const int workingFrequency = 15; //Hz
 
+unsigned long motorSpeedRpm = 0;
+int motorDirection = 0; // {-1;0;1} with 0 => stopped
+
 //// DC Motor control ////
 
 const int pinPotentiometer = 0; // A0
@@ -22,59 +31,6 @@ const int limitMotorPwm = 128; //Stay under 6V for 12V alim.
 DC_motor motor( pinEscPWM, pinEscDir, limitMotorPwm);  // PWM = Pin 11, DIR = Pin 12.
 int motorPwm = 0;
 
-//// DC Motor speed ////
-
-/*
-const float motorFilter = 0.5;       // [0,1] exponential filter parameter
-const int encoderCountRev = 212;
-const int pinEncoderAInterrupt = 3; //PD3
-const int pinEncoderB = 4;  //PD4
-
-const char registerMaskPIND = 0b00011000; //Show pin 
-volatile long motorTickCount = 0;
-unsigned long motorTimePrev = 0; //Some of those variables can be ignored/simplify
-unsigned long motorTimeNow = 0;
-unsigned long motorTimeDiff = 0;
-unsigned long motorSpeedRpm = 0;
-unsigned long motorSpeedRpmPrev = 0;
-unsigned long motorSpeedRpmRaw = 0;
-int motorDirection = 0; // {-1;0;1} with 0 => stopped
-
-
-void ISR_updateEncoder(){ //Direct access to register to optimize time consumption
-  if (((registerMaskPIND & PIND) == 0b00010000) or ((registerMaskPIND & PIND) == 0b00001000)){ 
-    motorTickCount++;
-  } else if (((registerMaskPIND & PIND) == 0b00000000) or ((registerMaskPIND & PIND) == 0b00011000)){
-    motorTickCount--;
-  }
-}
-
-
-void Update_MotorData(){
-
-  //Get motor rotation speed from tick count
-  motorTimeNow = millis();
-  motorTimeDiff = motorTimeNow - motorTimePrev;
-  motorTimePrev = motorTimeNow;
-  motorSpeedRpmPrev = motorSpeedRpm;
-  motorSpeedRpmRaw = ((motorTickCount > 0) ? motorTickCount : -motorTickCount) * 60000 / (encoderCountRev * motorTimeDiff); // 60000 => ms to s
-  motorTickCount = 0;
-
-  //Set motor rotation direction
-  if (motorSpeedRpm == 0) {
-    motorDirection = 0;
-  } else if (motorTickCount > 0) {
-    motorDirection = 1;
-  } else {
-    motorDirection = -1;
-  }
-  
-  //Filtering
-  motorSpeedRpm = motorSpeedRpmRaw * motorFilter + motorSpeedRpmPrev * (1.0 - motorFilter);
-}
-*/
-
-EncoderQuad encoderInertiaWheel(3,4,0.5,212);
 
 /*-----------------------*/
 /*   Arduino Processes   */
@@ -82,15 +38,7 @@ EncoderQuad encoderInertiaWheel(3,4,0.5,212);
 
 void setup() {
   Serial.begin(9600);
-/*
-  pinMode(pinEncoderAInterrupt, INPUT_PULLUP);
-  pinMode(pinEncoderB, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(pinEncoderAInterrupt), ISR_updateEncoder, CHANGE);
-  
-  motorTimePrev = millis();
-
-  motorTickCount = 0;
-*/
+  Encoder_Setup();
   timePrev = millis();
 }
 
@@ -101,11 +49,11 @@ void loop() {
     motorPwm = map(analogRead(A0), 0, 1023, 0, limitMotorPwm);
     motor.setSpeed(motorPwm);
 
-    encoderInertiaWheel.Update_MotorData();
+    Update_MotorData();
 
     Serial.print(motorPwm);
     Serial.print("\t");
-    Serial.println(encoderInertiaWheel.motorSpeedRpm);
+    Serial.println(motorSpeedRpm);
     
   }
 }
